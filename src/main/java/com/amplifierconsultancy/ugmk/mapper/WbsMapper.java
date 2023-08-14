e2f3ab53c7461cae506c4ac221f41f9305ab3e71
Boris Chernysh <bchernysh@lanit.ru>
2023-08-14 16:08:17 +0700

    Add BusinessObjectException throwing

--- src/main/java/com/amplifierconsultancy/ugmk/mapper/WbsMapper.java
+++ src/main/java/com/amplifierconsultancy/ugmk/mapper/WbsMapper.java
@@ -1,10 +1,13 @@
 package com.amplifierconsultancy.ugmk.mapper;
 
 import com.amplifierconsultancy.ugmk.dto.WbsDto;
+import com.primavera.integration.client.bo.BusinessObjectException;
 import com.primavera.integration.client.bo.object.WBS;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface WbsMapper {
-    WbsDto wbsToWbsDto(WBS wbs);
+    @Mapping(target = "children", ignore = true)
+    WbsDto wbsToWbsDto(WBS wbs) throws BusinessObjectException;
 }
